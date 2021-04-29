b13bf25ddfbf46bae12a8c1177de2c6eee3e0a28
Yami2195 <valesitzia@hotmail.com>
2021-04-29 19:02:02 +0200

    Salvataggio 1 (29/04/2021)

--- app/src/main/java/com/example/accio/MainActivity.java
+++ app/src/main/java/com/example/accio/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.accio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //associo alle variabili i propri id
+        username = findViewById(R.id.attrUsername);
+        password = findViewById(R.id.attrPassword);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
