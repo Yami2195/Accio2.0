02b79acf948f4be03cdb962b49133435370f9330
Yami2195 <valesitzia@hotmail.com>
2021-05-06 19:21:18 +0200

    06/05/2021 Grafica schermata registrazione

--- app/src/main/java/com/example/accio/MainActivity.java
+++ app/src/main/java/com/example/accio/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox showPass;
     EditText username, password;
     Button login;
     TextView registrati;
@@ -25,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.attrPassword);
         login = findViewById(R.id.login);
         registrati = findViewById(R.id.registrati);
+        showPass = findViewById(R.id.checkBox);
+
+        showPass.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
 
         registrati.setOnClickListener(new View.OnClickListener() {
             @Override
